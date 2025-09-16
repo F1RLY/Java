62f3a1ceb844220d33299d4f42c817e83632506c
F1RLY <firlyalamsudrajat@gmail.com>
2025-09-16 15:21:38 +0700

    tes

---
+++ latihan-array-collection/Arraymain.java
@@ -0,0 +1,15 @@
+public class Arraymain {
+    public static void main(String[] args) {
+        //cara 1
+        String[] nama = {"Alice", "Bob", "Charlie", "Diana"};
+        System.out.println(nama[3]);  
+
+        //cara 2
+        String[] daftarNama = new String[4];
+        daftarNama[0] = "Eve";
+        daftarNama[1] = "Frank";
+        daftarNama[2] = "Grace";
+        daftarNama[3] = "Hannah";
+        System.out.println(daftarNama[2]);
+    }
+}
